5f9255cc619143b3be32c79ccaca72182d108e7e
Gorlitz <panfyorov.ivan@mail.ru>
2023-03-28 23:42:14 +0300

    Sem 2

---
+++ C#_2/Program.cs
@@ -0,0 +1,32 @@
+﻿/*Напишите программу которая выводит третью цифру ззаданного числа
+или сообщает, что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6*/
+Console.Clear();
+int Prompt(string num3)
+{
+    Console.WriteLine($"{num3}");
+    return Convert.ToInt32(Console.ReadLine());
+}
+int num = Prompt ("Введите число: ");
+int num1(int num)
+{
+    while(num > 999)
+    {
+        num /=10;
+    }
+    return num % 10;
+}
+bool check(int num)
+{
+    if (num < 100)
+    return false;
+    else return true;
+}
+if (check(num) != true)
+Console.WriteLine("Третьей цифры нет");
+else 
+    {
+    Console.WriteLine($"Третья цифра числа {num} является {num1(num)}");
+    }
---
+++ C#_3/Program.cs
@@ -0,0 +1,19 @@
+﻿/*Напишите программу, которая принимает на вход цифру,
+обозначающую день недели, и проверяет, является ли этот день 
+выходным.
+6 -> да
+7 -> да
+1 -> нет*/
+Console.Clear();
+Console.Write("Введите цифру, обозначающую день недели: ");
+int dayNum = Convert.ToInt32(Console.ReadLine());
+void DayOfTheWeek(int dayNum) {
+    if(dayNum == 6 || dayNum == 7){
+        Console.WriteLine("(выходной) -> да");
+    }
+    else if (dayNum < 1 || dayNum > 7){
+        Console.WriteLine("Ошибка, такого дня недели несуществует");
+    }
+    else Console.WriteLine("(не выходной) -> нет");
+}
+DayOfTheWeek(dayNum);
